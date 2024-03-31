b4bc8c295f3a16caeb07fce02084a0b021e3a179
armaninformatics <ammangdeveloper@gmail.com>
2024-03-31 14:52:17 +0800

    tugas 11 belajar php

---
+++ products.php
@@ -0,0 +1,40 @@
+<?php
+$products = [
+    [
+        'nama' => "Laptop ASUS ROG",
+        'harga' => 15000000,
+        'deskripsi' => 'Ini adalah salah satu produk yang kami miliki',
+        'image' => "dist/img/avatar.png"
+    ],
+    [
+        'nama' => "Samsung S20",
+        'harga' => 10000000,
+        'deskripsi' => 'Ini adalah salah satu produk yang kami miliki',
+        'image' => "dist/img/avatar.png"
+    ],
+    [
+        'nama' => "Smart TV LG 55 inch",
+        'harga' => 12000000,
+        'deskripsi' => 'Ini adalah salah satu produk yang kami miliki',
+        'image' => "dist/img/avatar.png"
+    ],
+    [
+        'nama' => "Kamera Mirrorless Sony Alpha A7III",
+        'harga' => 20000000,
+        'deskripsi' => 'Ini adalah salah satu produk yang kami miliki',
+        'image' => "dist/img/avatar.png"
+    ],
+    [
+        'nama' => "Headphone Bose QuietComfort 35 II",
+        'harga' => 3000000,
+        'deskripsi' => 'Ini adalah salah satu produk yang kami miliki',
+        'image' => "dist/img/avatar.png"
+    ],
+    [
+        'nama' => "Mouse Gaming Logitech G502 Hero",
+        'harga' => 700000,
+        'deskripsi' => 'Ini adalah salah satu produk yang kami miliki',
+        'image' => "dist/img/avatar.png"
+    ]
+];
+?>
